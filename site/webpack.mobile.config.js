fbb8bdac79ded2553aab63f86731c20a16eecb8b
LIUYUAN076 <LIUYUAN076@pingan.com.cn>
2018-08-29 09:06:19 +0800

    调整样式，修改入口属性

--- site/webpack.mobile.config.js
+++ site/webpack.mobile.config.js
@@ -3,7 +3,7 @@ const cwd = path.join(__dirname, '../');
 
 module.exports = {
   PORT: 3331,
-  entry: {desktop: path.join(__dirname, './mobile/index.js')},
+  entry: {mobile: path.join(__dirname, './mobile/index.js')},
   output: {
     path: path.resolve(cwd, './dist/site/mobile'),
   }
